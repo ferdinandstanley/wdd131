c737a795de93c85289791514b834ef68ebd715dd
ferdinandstanley <ferdinandstanley20@gmail.com>
2024-11-28 03:57:11 -0700

    added

--- wdd1/scripts/getdates.js
+++
@@ -1,7 +0,0 @@
-// Get current year
-const currentYear = new Date().getFullYear();
-document.getElementById("currentyear").textContent = currentYear;
-
-// Get last modified date
-const lastModified = document.lastModified;
-document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
